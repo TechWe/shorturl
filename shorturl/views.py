dde53533cd0e02cd953d6d6004b6a9e34f958859
Vokey <code@lijinpeng.cn>
2018-07-10 18:04:42 +0800

    Complete views.go_to_origin

--- shorturl/views.py
+++ shorturl/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Url, Log
+
 def index(requests):
-    return HttpResponse('Hello from shorturl')
+    return HttpResponse('Hello from Shorturl. This is a simple url shortner by Jinpeng.')
+
+# Redirect to origin URL
+def go_to_origin(requests, keyword):
+    item = get_object_or_404(Url, keyword=keyword)
+    item.clicks += 1
+    item.save()
+    return redirect(item.origin_url)
