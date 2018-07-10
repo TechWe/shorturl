75cf52967946005cfb80848545a757021731d31e
Vokey <code@lijinpeng.cn>
2018-07-10 15:04:15 +0800

    update url conf, route shorturl to views.go_to_origin

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -1,9 +1,12 @@
 """ App shorturl URL Configuration
 """
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    # Route shorturl
+    # eg. /abc, keyword = 'abc'
+    re_path(r'^(?P<keyword>[\w][\w-]+)$', views.go_to_origin),
 ]
