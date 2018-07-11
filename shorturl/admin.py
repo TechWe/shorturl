9ad2f00605f875e7b36edc3f9aa8c58f56d67d33
Vokey <code@lijinpeng.cn>
2018-07-11 18:17:27 +0800

    update admin adn views

--- shorturl/admin.py
+++ shorturl/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Url, Log
 
-admin.site.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    readonly_fields = ['creator', 'creator_ip', 'timestamp']
+    fieldsets = [
+        ('Url Information', {
+            'fields': ['keyword', 'origin_url', 'title']
+            }),
+        ('Clicks', {'fields': ['clicks']}),
+        ('Creator Information', {'fields': readonly_fields})
+    ]
+admin.site.register(Url, UrlAdmin)
+
 admin.site.register(Log)
--- shorturl/views.py
+++ shorturl/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse
 from .models import Url, Log
 
 def index(requests):
-    return HttpResponse('Hello from Shorturl. This is a simple url shortner by Jinpeng.')
+    return HttpResponse('Hello from Shorturl. This is a simple url shortner by Jinpeng.' + requests.user.get_username())
 
 # Redirect to origin URL
 def go_to_origin(requests, keyword):
     item = get_object_or_404(Url, keyword=keyword)
+    # Url.clicks add 1
     item.clicked()
     # Return http301 redirect
     return redirect(item.origin_url, permanent=True)
