d64f25570442b0d8db5bdd0bb016bdbfd9de4bcc
Vokey <code@lijinpeng.cn>
2018-07-11 10:11:22 +0800

    add app_name, improve regex

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -4,9 +4,11 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'shorturl'
+
 urlpatterns = [
-    path('', views.index),
     # Route shorturl
     # eg. /abc, keyword = 'abc'
     re_path(r'^(?P<keyword>[\w]+[\w-]*)$', views.go_to_origin),
+    re_path(r'^$', views.index),
 ]
