3a3d497517f19cf9c415579486cb82eb73e40fc2
Vokey <code@lijinpeng.cn>
2018-07-12 00:07:25 +0800

    complete function in shorurl.views

--- shorturl/views.py
+++ shorturl/views.py
@@ -4,12 +4,26 @@ from django.http import HttpResponse
 from .models import Url, Log
 
 def index(requests):
-    return HttpResponse('Hello from Shorturl. This is a simple url shortner by Jinpeng.' + requests.user.get_username())
+    get_header = lambda meta, header, default: meta[header] if header in meta else default
+    meta = requests.META
+    ip = get_header(meta, 'REMOTE_ADDR', '0.0.0.0')
+    referrer = get_header(meta, 'HTTP_REFERER', 'direct')
+    user_agent = get_header(meta, 'HTTP_USER_AGENT', '')
+
+    return HttpResponse(f'Hello from Shorturl. This is a simple url shortner by Jinpeng. \n{ip} \n{referrer} \n{user_agent}')
 
 # Redirect to origin URL
 def go_to_origin(requests, keyword):
     item = get_object_or_404(Url, keyword=keyword)
     # Url.clicks add 1
     item.clicked()
+    #
+    get_header = lambda meta, header, default: meta[header] if header in meta else default
+    meta = requests.META
+    ip = get_header(meta, 'REMOTE_ADDR', '0.0.0.0')
+    referrer = get_header(meta, 'HTTP_REFERER', 'direct')
+    user_agent = get_header(meta, 'HTTP_USER_AGENT', '')
+    log = Log(url=keyword, click_ip=ip, referrer=referrer, user_agent=user_agent)
+    log.save()
     # Return http301 redirect
     return redirect(item.origin_url, permanent=True)
